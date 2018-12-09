f883951b9c759345cbdfee2f8b00bb67d5e73acc
Jules Ponsaillé <jules.ponsaille@isep.fr>
2018-12-09 18:16:29 +0100

    adding the bot's logic to the api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const generateAnswer = require('../utils/bot/index')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/sendMessage', function(req, res, next) {
+  const { message } = req.body
+
+  return res.json({
+      "message": generateAnswer(message)
+  })
+})
 
 module.exports = router;
